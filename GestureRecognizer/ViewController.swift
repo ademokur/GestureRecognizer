ed94c57013c929192b038d3abe7c8a001775dd5f
Adem MAMA <amama15@Adem-MacBook-Pro.local>
2017-12-06 14:39:21 +0300

    Initial Commit

---
+++ GestureRecognizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GestureRecognizer
+//
+//  Created by Adem MAMA on 6.12.2017.
+//  Copyright © 2017 Adem MAMA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
